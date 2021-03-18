30d9f99578f0268ca665ca1ea4a604cd8bb798b2
aymkh <aymen4real@gmail.com>
2021-03-18 21:39:14 +0100

    Delete levelKeys.js

--- public/js/levelKeys.js
+++
@@ -1,9 +0,0 @@
-const levelKeys = [
-    "CTF[|/|1$3]",
-    "CTF[90531653162858]",
-    "CTF[havefun]",
-    "CTF[01110011 01100001 01101000 01101001 01110100]",
-    "CTF[UQ6rHtT]"
-]
-
-module.exports = levelKeys
